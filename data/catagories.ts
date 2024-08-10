cbf2ce3c4907ef6f730953291efa0d6a5584d8ff
Ahmet Can Altıntaş <153134765+ahmetcnaltintas@users.noreply.github.com>
2024-08-11 01:24:43 +0300

    first commit

---
+++ data/catagories.ts
@@ -0,0 +1,44 @@
+const destinationCategories = [
+    {
+        title: "Tümü",
+        iconName: "hiking"
+    },
+    {
+        title: "Plaj",
+        iconName: "beach"
+    },
+    {
+        title: "Dağ",
+        iconName: "terrain"
+    },
+    {
+        title: "Şehir",
+        iconName: "city"
+    },
+    {
+        title: "Forests",
+        iconName: "tree"
+    },
+    {
+        title: "Lakes",
+        iconName: "swim"
+    },
+    {
+        title: "Historical Sites",
+        iconName: "castle"
+    },
+    {
+        title: "Historical Parks",
+        iconName: "pine-tree"
+    },
+    {
+        title: "Islands",
+        iconName: "island"
+    },
+    {
+        title: "Deserts",
+        iconName: "weather-sunny"
+    },
+];
+
+export default destinationCategories;
