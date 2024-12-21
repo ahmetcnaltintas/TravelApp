dbc96a8080c02bd2471559ce92702d4db4b55a8e
Ahmet Can Altıntaş <153134765+ahmetcnaltintas@users.noreply.github.com>
2024-12-21 14:00:55 +0300

    asd

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -38,7 +38,7 @@ function RootLayoutNav() {
       if (isFirstLaunch) {
         router.replace('/loading');
       } else if (user) {
-        router.replace('/(drawer)/(tabs)');
+        router.replace('/(drawer)');
       } else {
         router.replace('/(auth)/login');
       }
